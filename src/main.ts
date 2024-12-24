c7addc0a6537cbe4e91c4fee53a791b903c76352
brunoBrian <bruno.brian@vemprojuca.com>
2024-12-23 22:36:02 -0300

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,13 @@ import { AppModule } from "./app.module";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Enable CORS
+  app.enableCors({
+    origin: true, // Allow all origins
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    credentials: true,
+  });
+
   // Add global prefix
   app.setGlobalPrefix("api");
 
