947d41b99e2c84533b130a8342b96591c19e489c
brunoBrian <bruno.brian@vemprojuca.com>
2024-12-26 18:56:10 -0300

    fix

--- src/shared/services/email.service.ts
+++ src/shared/services/email.service.ts
@@ -9,7 +9,7 @@ export class EmailService {
     this.transporter = nodemailer.createTransport({
       host: process.env.SMTP_HOST,
       port: parseInt(process.env.SMTP_PORT),
-      secure: true,
+      secure: false,
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
